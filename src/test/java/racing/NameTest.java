4fdc0e8a867117b823605a322d4e3460e6823489
DESKTOP-F7EIOPM\rldhk <sdaff1004@gmail.com>
2022-03-03 21:00:35 +0900

    refactor : test method naming 변경

--- src/test/java/racing/NameTest.java
+++ src/test/java/racing/NameTest.java
@@ -8,12 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class NameTest {
 
     @Test
-    void name_toString() {
-        assertThat(new Name("sport").toString()).isEqualTo("sport");
-    }
-
-    @Test
-    void IllegalArgumentException_공백_null_5자리_초과() {
+    void create_공백_null_5자리_초과_IllegalArgumentExceptionThrown() {
         assertThatThrownBy(() -> new Name("")).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Name(null)).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Name("super")).isInstanceOf(IllegalArgumentException.class);
