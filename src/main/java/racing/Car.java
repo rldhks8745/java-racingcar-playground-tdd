59d8c58ca18150c074258546af105b9aa2f930be
rldhks8745 <sdaff1004@gmail.com>
2022-03-06 23:56:27 +0900

    feat : String, int 생성자 추가

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -8,8 +8,12 @@ public class Car {
     private final Position position;
 
     public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int position) {
         this.name = new Name(name);
-        this.position = new Position(0);
+        this.position = new Position(position);
     }
 
     public void move(int number) {
